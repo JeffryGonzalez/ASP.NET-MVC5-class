366d16f32a6e4963f17ab014c7ae0b4e7bd7b202
Jeffry Gonzalez <jeff@hypertheory.com>
2014-12-15 20:25:28 +0000

    added unit test project

---
+++ MvcUnitTests/ControllerTests/NewsLetterControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Starter.Controllers;
+
+namespace MvcUnitTests.ControllerTests
+{
+    [TestClass]
+    public class NewsLetterControllerTests
+    {
+        [TestMethod]
+        public void NewReturnsAView()
+        {
+            var controller = new NewsLetterRegistrationController();
+
+            var view = controller.New() as ViewResult;
+
+            Assert.IsNotNull(view);
+
+        }
+    }
+}
---
+++ Starter/Controllers/RegistrationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Starter.Controllers
+{
+    public class NewsLetterRegistrationController : Controller
+    {
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
