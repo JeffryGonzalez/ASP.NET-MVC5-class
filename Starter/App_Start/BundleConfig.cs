a19084a6b62e8aad3afd5d41d4cf892188ba7315
Jeffry Gonzalez <jeff@hypertheory.com>
2014-12-15 17:53:36 +0000

    Added optimization and bootstrap

---
+++ Starter/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Starter.App_Start
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // css bundles
+            bundles.Add(new StyleBundle(BundleNames.BootStrapBase).Include("~/Content/bootstrap.css"));
+            // js bundles
+        }
+    }
+
+    public static class BundleNames
+    {
+        public static string BootStrapBase = "~/bundles/bootstrap";
+    }
+}
--- Starter/Global.asax.cs
+++ Starter/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Starter.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Starter
@@ -13,6 +15,7 @@ namespace Starter
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
