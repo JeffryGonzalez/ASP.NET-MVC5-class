2388784e696c103c0518a281e5dbb8530b6e6c15
Jeffry Gonzalez <jeff@hypertheory.com>
2014-12-15 22:09:52 -0500

    more helpers

--- Starter/Code/CustomHtmlHelpers.cs
+++ Starter/Code/CustomHtmlHelpers.cs
@@ -8,25 +8,61 @@ using System.Web.Mvc.Html;
 
 namespace Starter.Code
 {
-    public static class CustomHtmlHelpers
-    {
-        public static HtmlString MakeInputGroup<TModel,TProperty> (this HtmlHelper<TModel> helper, Expression<Func<TModel,TProperty>> expression) where TModel : class
-        {
-            var metaData = ModelMetadata.FromLambdaExpression<TModel, TProperty>(expression, helper.ViewData);
-            var labelText = metaData.DisplayName ??
-                            (metaData.PropertyName ?? ExpressionHelper.GetExpressionText(expression));
-            var parentDiv = new TagBuilder("div");
-            parentDiv.AddCssClass("form-group");
+	public static class CustomHtmlHelpers
+	{
+		public static HtmlString GetDisplayNameFor<TModel, TProperty>(this HtmlHelper<TModel> helper,
+			Expression<Func<TModel, TProperty>> expression) where TModel : class
+		{
+			var metaData = GetMetaData(helper, expression);
+			var labelText = GetLabelText(expression, metaData);
+			return new HtmlString(labelText);
+		}
 
-            var label = new TagBuilder("label");
-            label.SetInnerText(labelText);
-            parentDiv.InnerHtml += label.ToString();
+	
 
-            var input = helper.TextBox(metaData.PropertyName, metaData.Model, new {@class="form-control"}).ToString();
-            parentDiv.InnerHtml += input.ToString();
+		public static HtmlString MakeInputTextGroup<TModel, TProperty>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression) where TModel : class
+		{
+			var metaData = GetMetaData(helper, expression);
+			var labelText = GetLabelText(expression, metaData);
 
-            return new HtmlString(parentDiv.ToString());
+			var parentDiv = MakeFormGroupDiv();
 
-        }
-    }
+			parentDiv.InnerHtml += MakeLabel(labelText).ToString(); 
+
+			var input = helper.TextBox(metaData.PropertyName, metaData.Model, new { @class = "form-control" }).ToString();
+			parentDiv.InnerHtml += input;
+
+			var alertDiv = new TagBuilder("div");
+			alertDiv.AddCssClass("help-block");
+			alertDiv.InnerHtml += helper.ValidationMessage(metaData.PropertyName);
+
+			parentDiv.InnerHtml += alertDiv.ToString();
+			return new HtmlString(parentDiv.ToString());
+
+		}
+
+		private static TagBuilder MakeLabel(string labelText)
+		{
+			var label = new TagBuilder("label");
+			label.SetInnerText(labelText);
+			return label;
+		}
+
+		private static TagBuilder MakeFormGroupDiv()
+		{
+			var parentDiv = new TagBuilder("div");
+			parentDiv.AddCssClass("form-group");
+			return parentDiv;
+		}
+
+		private static ModelMetadata GetMetaData<TModel, TProperty>(HtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression) where TModel : class
+		{
+			return ModelMetadata.FromLambdaExpression<TModel, TProperty>(expression, helper.ViewData);
+		}
+		private static string GetLabelText<TModel, TProperty>(Expression<Func<TModel, TProperty>> expression, ModelMetadata metaData) where TModel : class
+		{
+			return metaData.DisplayName ??
+						 (metaData.PropertyName ?? ExpressionHelper.GetExpressionText(expression));
+		}
+	}
 }
--- Starter/Models/NewsletterRegistration/NewRegistration.cs
+++ Starter/Models/NewsletterRegistration/NewRegistration.cs
@@ -4,14 +4,15 @@ using Starter.Code;
 
 namespace Starter.Models.NewsletterRegistration
 {
-    public class NewRegistration
-    {
-        [Required(ErrorMessage = "First Name Must Be Provided")]
-        public string FirstName { get; set; }
-        [Required]
-        public string LastName { get; set; }
-        [Required]
-        public string EmailAddress { get; set; }
-        public bool? ReceiveDailyEmail { get; set; }
-    }
+	public class NewRegistration
+	{
+		[Required(ErrorMessage = "First Name Must Be Provided")]
+		public string FirstName { get; set; }
+		[Required]
+		public string LastName { get; set; }
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string EmailAddress { get; set; }
+		public bool ReceiveDailyEmail { get; set; }
+	}
 }
